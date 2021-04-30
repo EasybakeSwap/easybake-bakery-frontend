420cc351379adb9d9a4cc7d8d3b4b264c6b7b966
Lucus <lucusra@protonmail.com>
2021-05-01 00:59:58 +1000

    Quick access to contract addresses

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -51,6 +51,36 @@ const config: MenuEntry[] = [
     ],
   },
   {
+    label: 'Contracts (Rinkeby)',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'OvenToken.sol',
+        href: 'https://rinkeby.etherscan.io/address/0x95b3B3E534DA76CBa9DEd682eedf8724eF8e38aB#code',
+      },
+      {
+        label: 'SugarBar.sol',
+        href: 'https://rinkeby.etherscan.io/address/0x248Dc87bf9e0d4a499d38E76152ee38527C4eA89#code',
+      },
+      {
+        label: 'MasterChef.sol',
+        href: 'https://rinkeby.etherscan.io/address/0xBA60c36f2B23E1F4Ef7bBa10F897340060c62339#code',
+      },
+      {
+        label: 'Multicall.sol',
+        href: 'https://rinkeby.etherscan.io/address/0x1605d633e30f430d54162232571475c620c69da5#code',
+      },
+      {
+        label: 'EasyBakeFactory.sol',
+        href: 'https://rinkeby.etherscan.io/address/0x77e43eEA4e7d475676d1af631A641C12BEE4dcE3#code',
+      },
+      {
+        label: 'EasyBakeRouter02.sol',
+        href: 'https://rinkeby.etherscan.io/address/0x6eb6f7F41714B74Dc13415f11F5B5a29dd9214D2#code',
+      }
+    ],
+  },
+  {
     label: 'More',
     icon: 'MoreIcon',
     items: [
