920c8619d9e97465f0ad266f7199806dae0622ca
Lucus <lucusra@protonmail.com>
2021-06-01 14:15:48 +1000

    Page fade transitions

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import { Box } from 'easybake-uikit'
 import Container from '../layout/Container'
 
+const Load = keyframes`{
+  0% {
+    opacity: 0%;
+  }
+  100% {
+    opacity: 100%;
+  }
+}`;
+
 const Outer = styled(Box)<{ background?: string }>`
   background: ${({ theme, background }) => background || theme.colors.gradients.bubblegum};
+  animation: ${Load} 300ms ease-in forwards;
 `
 
 const Inner = styled(Container)`
--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router'
 import { customMeta, DEFAULT_META } from 'config/constants/meta'
 // import { usePriceOvenUsdc } from 'state/hooks'
 import Container from './Container'
 
+const Load = keyframes`{
+  0% {
+    opacity: 0%;
+  }
+  100% {
+    opacity: 100%;
+  }
+}`;
+
+
 const StyledPage = styled(Container)`
   min-height: calc(100vh - 64px);
   padding-top: 16px;
   padding-bottom: 16px;
+  animation: ${Load} 300ms ease-in forwards;
 
   ${({ theme }) => theme.mediaQueries.sm} {
     padding-top: 24px;
